a90bd17a2c1c9316bf52ba06b4524a1a876cc5ae
JingYuan <jingyuan0926@gmail.com>
2023-12-20 14:31:16 +0800

    Array working wont show at react

--- contract/scripts/getInfo.js
+++ contract/scripts/getInfo.js
@@ -2,14 +2,14 @@
 
 const hre = require("hardhat");
 
-const CONTRACT_ADDRESS = "0x32697dC6d02dFB5DAA68fa8fFcD0a362b93fF96a"
+const CONTRACT_ADDRESS = "0x4B1eacD1FAD89C0DB36aed5E44136b41De73d333"
 
 async function main() {
     // Getting the contract factory
     const contract = await hre.ethers.getContractAt("TicketNFT", CONTRACT_ADDRESS);
 
     // Deploying the contract
-    const info = await contract.getTicketInfo(1);
+    const info = await contract.transactions(0);
     
     console.log(info);
 }
