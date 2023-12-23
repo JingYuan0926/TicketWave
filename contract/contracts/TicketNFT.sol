7eef91cf48a54d39d1a325771c3046f14a81b8d2
JingYuan <jingyuan0926@gmail.com>
2023-12-24 00:25:41 +0800

    More Complete

--- contract/contracts/TicketNFT.sol
+++ contract/contracts/TicketNFT.sol
@@ -4,22 +4,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
 // Inherit from the OpenZeppelin ERC721 contract
 contract TicketNFT is ERC721URIStorage {
-
     // Track number of tickets minted
     uint256 public tokenCounter;
-     // Set ticket price
-    uint256 public ticketPrice = 0.01 ether; 
+    // Set ticket price
+    uint256 public ticketPrice = 0.01 ether;
     // Store address of owner
     address payable public owner;
     // Maxiumum number of tickets that can be minted
     uint256 public maxSupply;
-    // Hold ticket info
-    
+
     // For buy now pay later
-     struct PaymentPlan {
+    struct PaymentPlan {
         uint256 totalPaid;
         uint256 monthlyPayment;
         uint8 monthsPaid;
@@ -27,8 +24,10 @@ contract TicketNFT is ERC721URIStorage {
     }
 
     mapping(uint256 => PaymentPlan) public paymentPlans;
+    mapping(address => bool) public hasPurchased;
+    mapping(address => uint256[]) public userTickets;
 
-    // This event is emitted when a ticket is purchased, and includes the ticket ID and purchaser address 
+    // This event is emitted when a ticket is purchased, and includes the ticket ID and purchaser address
     event TicketPurchased(uint256 indexed ticketId, address indexed purchaser);
     event MonthlyPaymentMade(uint256 indexed ticketId, uint256 amount);
 
@@ -36,41 +35,81 @@ contract TicketNFT is ERC721URIStorage {
     constructor(uint256 _maxSupply) ERC721("FirstTicket", "FTCKT") {
         owner = payable(msg.sender);
         tokenCounter = 0;
-        maxSupply = _maxSupply; 
+        maxSupply = _maxSupply;
+    }
+
+    function getUserTickets(address user) public view returns (uint256[] memory) {
+    return userTickets[user];
+    }
+
+    function areAllTicketsFullyPaid(address user) public view returns (bool) {
+        for (uint i = 0; i < userTickets[user].length; i++) {
+            if (!paymentPlans[userTickets[user][i]].fullyPaid) {
+                return false;
+            }
+        }
+        return true;
     }
 
     // Create a new ticket and only allow the owner to call this function
-    function createTicket(address recipient, string memory tokenURI) internal returns (uint256) {
+    function createTicket(
+        address recipient,
+        string memory tokenURI
+    ) internal returns (uint256) {
         require(tokenCounter < maxSupply, "Maximum ticket supply reached");
         uint256 newTicketId = tokenCounter;
         // Function inherited from ERC721 contract, use to mint a new NFT
         _safeMint(recipient, newTicketId);
         // Set the metadata URI for the new token
-        _setTokenURI(newTicketId, tokenURI);  
+        _setTokenURI(newTicketId, tokenURI);
+        userTickets[recipient].push(newTicketId);
         tokenCounter++;
         return newTicketId;
     }
 
     // Allow anyone to purchase a ticket as long as they send enough ETH
-    function purchaseTickets(address recipient, string memory tokenURI, uint256 quantity) external payable {
+    function purchaseTickets(
+        address recipient,
+        string memory tokenURI,
+        uint256 quantity
+    ) external payable {
         require(quantity > 0 && quantity <= 6, "Purchase 1-6 tickets");
         require(msg.value >= (ticketPrice * quantity), "Insufficient ETH");
         require(tokenCounter + quantity <= maxSupply, "Max supply exceeded");
-    for (uint256 i = 0; i < quantity; i++) {
+        require(!hasPurchased[msg.sender], "Sender already owns a ticket");
+
+        for (uint256 i = 0; i < quantity; i++) {
             uint256 newTicketId = createTicket(recipient, tokenURI);
+            // Mark the ticket as fully paid since it's a full payment
+            paymentPlans[newTicketId] = PaymentPlan({
+                totalPaid: ticketPrice,
+                monthlyPayment: 0, // No monthly payments needed for full purchase
+                monthsPaid: 0, // No months paid needed for full purchase
+                fullyPaid: true // Mark as fully paid
+            });
             emit TicketPurchased(newTicketId, msg.sender);
         }
+
+        hasPurchased[msg.sender] = true;
 
         uint256 refund = msg.value - (ticketPrice * quantity);
         if (refund > 0) {
             payable(msg.sender).transfer(refund);
         }
     }
-    
-    function purchaseTicketsWithBNPL(address recipient, string memory tokenURI, uint256 quantity) external payable {
+
+    function purchaseTicketsWithBNPL(
+        address recipient,
+        string memory tokenURI,
+        uint256 quantity
+    ) external payable {
         require(quantity > 0 && quantity <= 6, "Purchase 1-6 tickets");
-        require(msg.value >= ((ticketPrice * quantity) / 4), "Insufficient ETH for first installment");
+        require(
+            msg.value >= ((ticketPrice * quantity) / 4),
+            "Insufficient ETH for first installment"
+        );
         require(tokenCounter + quantity <= maxSupply, "Max supply exceeded");
+        require(!hasPurchased[msg.sender], "Sender already owns a ticket");
 
         for (uint256 i = 0; i < quantity; i++) {
             uint256 newTicketId = createTicket(recipient, tokenURI);
@@ -83,6 +122,8 @@ contract TicketNFT is ERC721URIStorage {
             emit TicketPurchased(newTicketId, msg.sender);
         }
 
+        hasPurchased[msg.sender] = true;
+
         uint256 refund = msg.value - ((ticketPrice * quantity) / 4);
         if (refund > 0) {
             payable(msg.sender).transfer(refund);
@@ -93,7 +134,10 @@ contract TicketNFT is ERC721URIStorage {
         PaymentPlan storage plan = paymentPlans[ticketId];
         require(!plan.fullyPaid, "Ticket already fully paid");
         require(plan.monthsPaid < 4, "All installments already paid");
-        require(msg.value >= plan.monthlyPayment, "Insufficient payment amount");
+        require(
+            msg.value >= plan.monthlyPayment,
+            "Insufficient payment amount"
+        );
 
         plan.totalPaid += msg.value;
         plan.monthsPaid++;
@@ -105,17 +149,23 @@ contract TicketNFT is ERC721URIStorage {
         emit MonthlyPaymentMade(ticketId, msg.value);
 
         // Refund overpayment
-        uint256 overpayment = plan.totalPaid > ticketPrice ? plan.totalPaid - ticketPrice : 0;
+        uint256 overpayment = plan.totalPaid > ticketPrice
+            ? plan.totalPaid - ticketPrice
+            : 0;
         if (overpayment > 0) {
             payable(msg.sender).transfer(overpayment);
             plan.totalPaid = ticketPrice;
         }
     }
 
-    function getTicketInfo(uint256 ticketId) public view returns (string memory) {
-    // This will revert if the ticket does not exist
-     // Check if ticket exists. It will revert if not.
-    ownerOf(ticketId); 
-    return tokenURI(ticketId);
-}
+    function isTicketOnBNPL(uint256 ticketId) public view returns (bool) {
+        PaymentPlan storage plan = paymentPlans[ticketId];
+        // A ticket is considered to be on BNPL if there's a payment plan with less than 4 months paid
+        // and it is not fully paid.
+        return plan.monthsPaid > 0 && plan.monthsPaid < 4 && !plan.fullyPaid;
+    }
+
+    function resetPurchaseStatus(address user) public {
+        hasPurchased[user] = false;
+    }
 }
--- contract/test/TicketNFT-test.js
+++ contract/test/TicketNFT-test.js
@@ -1,15 +1,16 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("TicketNFT", function () {
+describe("TicketNFT Contract", function () {
     let TicketNFT;
     let ticketNFT;
     let owner;
     let addr1;
     let addr2;
     let addrs;
-    const number =6;
-    // 1,4,5 wont work
+    const ticketURI = "https://ipfs.io/ipfs/Qm";
+    const ticketPrice = ethers.utils.parseEther("0.01");
+    const monthlyPayment = ethers.utils.parseEther("0.0025");
 
     beforeEach(async function () {
         TicketNFT = await ethers.getContractFactory("TicketNFT");
@@ -23,12 +24,111 @@ describe("TicketNFT", function () {
         });
     });
 
-    describe("Minting Tickets", function () {
-        it("Should mint and assign a new ticket", async function () {
-            await ticketNFT.purchaseTicketsWithBNPL(addr1.address, "https://ipfs.io/ipfs/QmZGCJvLN8gjPrDbsYo5VG8qBmwwj2b9DVSVzbsH5Tbu8j",number,{ value: ethers.utils.parseEther(((0.1 * number) / 3).toString()) });
-            expect(await ticketNFT.ownerOf(0)).to.equal(addr1.address);
+    describe("Purchase Tickets", function () {
+        it("Should allow users to purchase tickets", async function () {
+            await ticketNFT.connect(addr1).purchaseTickets(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.01") });
+            expect(await ticketNFT.hasPurchased(addr1.address)).to.be.true;
         });
-    
+
+        it("Should not allow purchasing more than once", async function () {
+            await ticketNFT.connect(addr1).purchaseTickets(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.01") });
+            await expect(ticketNFT.connect(addr1).purchaseTickets(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.01") }))
+                .to.be.revertedWith("Sender already owns a ticket");
+        });
     });
 
+    describe("BNPL Functionality", function () {
+        it("Should allow users to purchase tickets with BNPL", async function () {
+            await ticketNFT.connect(addr1).purchaseTicketsWithBNPL(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.0025") });
+            const ticketId = await ticketNFT.userTickets(addr1.address, 0);
+            expect(await ticketNFT.isTicketOnBNPL(ticketId)).to.be.true;
+        });
+
+        it("Should see if the user is on bnpl", async function () {
+            // Purchase a ticket with BNPL
+            await ticketNFT.connect(addr1).purchaseTicketsWithBNPL(
+                addr1.address,
+                "tokenURI",
+                1,
+                { value: ethers.utils.parseEther("0.0025") }
+            );
+
+            // Get the ticket ID for the first ticket. Assuming `userTickets` is a public mapping
+            // Get the ticket ID for the first ticket. Assuming `userTickets` is a public mapping
+            const ticketId = await ticketNFT.userTickets(addr1.address, 0);
+
+
+            // Check if the ticket is on a BNPL plan
+            expect(await ticketNFT.isTicketOnBNPL(ticketId)).to.be.true;
+        });
+
+        it("Should allow users to make monthly payments", async function () {
+            await ticketNFT.connect(addr1).purchaseTicketsWithBNPL(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.0025") });
+            const ticketId = await ticketNFT.userTickets(addr1.address, 0);
+            await ticketNFT.connect(addr1).makeMonthlyPayment(ticketId, { value: ethers.utils.parseEther("0.0025") });
+            const paymentPlan = await ticketNFT.paymentPlans(ticketId);
+            expect(paymentPlan.monthsPaid).to.equal(2);
+        });
+
+    });
+
+    describe("BNPL Full Payment", function () {
+        it("Should mark the ticket as fully paid after all payments are made", async function () {
+            // addr1 purchases a ticket with BNPL
+            await ticketNFT.connect(addr1).purchaseTicketsWithBNPL(
+                addr1.address,
+                ticketURI,
+                1, // quantity
+                { value: monthlyPayment }
+            );
+
+            // Making the rest of the payments
+            for (let i = 0; i < 3; i++) { // 3 more payments to complete the BNPL
+                await ticketNFT.connect(addr1).makeMonthlyPayment(0, { value: monthlyPayment });
+            }
+
+            // Check if the ticket is fully paid
+            const paymentPlan = await ticketNFT.paymentPlans(0);
+            expect(paymentPlan.fullyPaid).to.equal(true);
+
+            // Check if all tickets of addr1 are fully paid
+            const allFullyPaid = await ticketNFT.areAllTicketsFullyPaid(addr1.address);
+            expect(allFullyPaid).to.equal(true);
+        });
+    });
+
+    describe("Reset Function", function () {
+
+        it("Can buy again", async function () {
+            await ticketNFT.connect(addr1).purchaseTickets(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.01") });
+            await ticketNFT.connect(addr1).resetPurchaseStatus(addr1.address);
+            await ticketNFT.connect(addr1).purchaseTickets(addr1.address, "tokenURI", 1, { value: ethers.utils.parseEther("0.01") });
+            expect(await ticketNFT.hasPurchased(addr1.address)).to.be.true;
+        });
+    });
+
+    describe("BNPL Functionality", function () {
+        it("Should get BNPL details for a user", async function () {
+            // User addr1 buys a ticket with BNPL
+            await ticketNFT.connect(addr1).purchaseTicketsWithBNPL(
+                addr1.address,
+                "tokenURI",
+                1,
+                { value: ethers.utils.parseEther("0.0025") } // 25% of ticket price
+            );
+
+            // Retrieve ticket ID
+            const ticketIds = await ticketNFT.getUserTickets(addr1.address);
+            const ticketId = ticketIds[0];
+            
+            const paymentPlan = await ticketNFT.paymentPlans(ticketId);
+
+        // Assertions to verify BNPL details
+        expect(paymentPlan.totalPaid).to.equal(ethers.utils.parseEther("0.0025"));
+        expect(paymentPlan.monthlyPayment).to.equal(ethers.utils.parseEther("0.0025"));
+        expect(paymentPlan.monthsPaid).to.equal(1);
+        expect(paymentPlan.fullyPaid).to.be.false;
+
+        });
+    });
 });
