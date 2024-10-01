328b0138d93b0b044f96b03e216853060d84fc2a
JingYuan <jingyuan0926@gmail.com>
2024-10-01 13:02:09 +0800

    Added NextUi config and test button

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button >
+        Test NextUI Button
+      </Button>
+    </div>
+  );
+}
