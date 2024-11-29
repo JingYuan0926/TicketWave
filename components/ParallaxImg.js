4e216de64560897f2d2af3f28fec2d36ca859df7
Marcussy34 <marcus.tanchiyau@gmail.com>
2024-11-29 19:15:12 +0800

    added landing page from hover.dev

---
+++ components/ParallaxImg.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import {
+  useScroll,
+  useTransform,
+  motion,
+  useMotionTemplate,
+} from "framer-motion";
+
+const ParallaxImg = ({ className, alt, src, start, end }) => {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: [`${start}px end`, `end ${end * -1}px`],
+  });
+
+  const opacity = useTransform(scrollYProgress, [0.75, 1], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0.75, 1], [1, 0.85]);
+
+  const y = useTransform(scrollYProgress, [0, 1], [start, end]);
+  const transform = useMotionTemplate`translateY(${y}px) scale(${scale})`;
+
+  return (
+    <motion.img
+      src={src}
+      alt={alt}
+      className={className}
+      ref={ref}
+      style={{ transform, opacity }}
+    />
+  );
+};
+
+export default ParallaxImg;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "../styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { ThirdwebProvider } from "thirdweb/react";
+import { useRouter } from "next/router"; // Import useRouter hook
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter(); // Get the current route
+
+  // Check if the current page is the landing page (you can change this if your landing page has a different route)
+  const isLandingPage = router.pathname === "/landingpage";
+
   return (
     <ThirdwebProvider>
       <NextUIProvider>
         <div className="min-h-screen flex flex-col overflow-x-hidden">
-          <Navbar />
+          {/* Only render Navbar if the current page is not the landing page */}
+          {!isLandingPage && <Navbar />}
           <main className="flex-1">
             <Component {...pageProps} />
           </main>
